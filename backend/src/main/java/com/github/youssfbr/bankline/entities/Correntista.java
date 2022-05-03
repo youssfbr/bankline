5c853e877176d85c0b1dd86b8cc2b827a1b91499
Alisson Youssf <youssfbr@gmail.com>
2022-05-03 11:49:00 -0300

    Entities, Profile, H2, Postgres, Spring Security

--- backend/src/main/java/com/github/youssfbr/bankline/entities/Correntista.java
+++ backend/src/main/java/com/github/youssfbr/bankline/entities/Correntista.java
@@ -3,14 +3,25 @@ package com.github.youssfbr.bankline.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_correntista")
 public class Correntista {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 15, unique = true)
     private String cpf;
+
+    @Column(length = 40)
     private String nome;
 
+    @Embedded
     private Conta conta;
 
 }
--- backend/src/main/java/com/github/youssfbr/bankline/entities/Movimentacao.java
+++ backend/src/main/java/com/github/youssfbr/bankline/entities/Movimentacao.java
@@ -4,16 +4,24 @@ import com.github.youssfbr.bankline.entities.enums.MovimentacaoTipo;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_movimentacao")
 public class Movimentacao {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDateTime dataHora;
     private String descricao;
     private Double valor;
+
+    @Enumerated(EnumType.STRING)
     private MovimentacaoTipo tipo;
 
 }
