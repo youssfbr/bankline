9c71c47537fe5da15e213bc0f51a112cffdc83bb
Alisson Youssf <youssfbr@gmail.com>
2022-05-03 10:54:53 -0300

    Project created

---
+++ backend/src/test/java/com/github/youssfbr/bankline/BanklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
