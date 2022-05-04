76629aa144fa3504a5fb1d17530260c98ad75092
Alisson Youssf <youssfbr@gmail.com>
2022-05-04 11:06:00 -0300

    Angular - bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { MovimentacaoListComponent } from './pages/movimentacao/movimentacao-list/movimentacao-list.component';
 import { MovimentacaoFormComponent } from './pages/movimentacao/movimentacao-form/movimentacao-form.component';
 
@@ -14,7 +17,8 @@ import { MovimentacaoFormComponent } from './pages/movimentacao/movimentacao-for
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
